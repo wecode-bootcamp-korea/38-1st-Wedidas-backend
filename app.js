31c8a184aa5d65b84cb52fa3f98196657533b9d4
csongin <cheonsong37@gmail.com>
2022-10-17 17:17:31 +0900

    [modify] db connection 완료

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const morgan =require('morgan');
+const appDataSource = require('./models/dataSource');
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
@@ -15,6 +20,11 @@ const PORT = process.env.PORT;
 const start = async () => {
   try {
     app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+    appDataSource
+    .initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!");
+    })
   } catch (err) {
     console.error(err);
   }
