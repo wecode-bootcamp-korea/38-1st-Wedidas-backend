9930371a70052e788725e122179677110f162b8c
csongin <cheonsong37@gmail.com>
2022-10-20 15:11:00 +0900

    [remove] getUserById 코드 삭제

--- models/userDao.js
+++ models/userDao.js
@@ -16,22 +16,6 @@ const getUserByEmail = async (email) => {
 	return result[0];
 }
 
-const getUserById = async (id) => {
-	const result = await dataSource.query(`
-		SELECT 
-			id,
-			name,
-			email,
-			birthday,
-			phone_number
-		FROM users
-		WHERE id=?`, [id]
-	);
-
-	return result[0];
-}
-
 module.exports = { 
 	getUserByEmail,
-	getUserById
 }
